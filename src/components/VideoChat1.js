653363d0e440ce7b084f3cc92f837cfa1f682517
kalyankashaboina <kalyankashaboina07@gmail.com>
2025-05-23 21:22:43 +0530

    updated backend URL

--- src/components/VideoChat1.js
+++ src/components/VideoChat1.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useRef, useState } from 'react';
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:5000');
+// const socket = io('http://localhost:5000');
+const socket = io('https://videocall-server-gb9t.onrender.com');
 
 function VideoChat() {
   const [yourSocketId, setYourSocketId] = useState('');
--- src/socket.js
+++ src/socket.js
@@ -2,6 +2,8 @@
 import { io } from 'socket.io-client';
 
 // Use the same URL your backend is running on
-const socket = io('http://localhost:5000'); // Replace with your backend URL
+// const socket = io('http://localhost:5000'); // Replace with your backend URL
+//https://videocall-server-gb9t.onrender.com
+const socket = io('https://videocall-server-gb9t.onrender.com');
 
 export default socket;
